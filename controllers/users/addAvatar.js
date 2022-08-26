2bc363a765063c8aae1c99d1fe050fdfaad76820
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-08-26 16:11:09 +0300

    change path and avatarName in addAvatars ctrl

--- controllers/users/addAvatar.js
+++ controllers/users/addAvatar.js
@@ -2,20 +2,26 @@ const { User } = require("../../models/user");
 const fs = require("fs/promises");
 const path = require("path");
 
-const avatarsDir = path.resolve("public", "avatars");
+const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
 
 const addAvatar = async (req, res) => {
-  const { path: tempUpload, originalname } = req.file;
-  const resultUpload = path.join(avatarsDir, originalname);
-
+  const { path: tmpUpload, originalname } = req.file;
+  const { _id } = req.user;
+  const avatarName = `${_id}_${originalname}`;
   try {
-    await fs.rename(tempUpload, resultUpload);
-    const { _id } = req.user;
-    const avatar = path.join("avatars", originalname);
-    await User.findByIdAndUpdate(_id, { $set: { avatarURL: avatar } });
-    res.json({ message: avatar });
+    const resultUpload = path.join(avatarsDir, avatarName);
+    console.log(resultUpload);
+    await fs.rename(tmpUpload, resultUpload);
+
+    const avatarURL = path.join("avatars", avatarName);
+
+    await User.findByIdAndUpdate(_id, { avatarURL });
+
+    res.json({ message: avatarURL });
   } catch (error) {
-    await fs.unlink(tempUpload);
+    console.log("catch");
+    await fs.unlink(tmpUpload);
+    throw error;
   }
 };
 
