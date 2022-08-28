939f44663ebdef61b0caedaa7584da737e89e893
Serhii Rubtsov <rubtsov1986@bigmir.net>
2022-08-28 11:16:18 +0300

    finish test for login controller

--- controllers/auth/login.test.js
+++ controllers/auth/login.test.js
@@ -1,25 +1,60 @@
 const express = require("express");
 const request = require("supertest");
 const { ctrlWrapper } = require("../../helpers");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-jest.setTimeout(15000);
+require("dotenv").config();
+const { DB_HOST, PORT = 3000 } = process.env;
 
 const { auth: ctrl } = require("../../controllers");
 
 const app = express();
 
-app.post("/login", ctrlWrapper(ctrl.login));
-
+app.use(cors());
 app.use(express.json());
 
+app.post("/login", ctrlWrapper(ctrl.login));
+
+const data = { email: "qwerty2@qwe.qwe", password: "12345" };
+
 describe("test login controller", () => {
-  beforeAll(() => app.listen(3000, () => console.log("start")));
+  beforeAll(() =>
+    mongoose
+      .connect(DB_HOST)
+      .then(() => {
+        console.log("Database connection successful");
+        app.listen(PORT, () => {
+          console.log(`Server running. Use our API on port: ${PORT}`);
+        });
+      })
+      .catch((error) => {
+        console.log(error.message);
+        process.exit(1);
+      })
+  );
 
-  test("correct request", async () => {
-    const data = { email: "qwerty2@qwe.qwe", password: "12345" };
+  test("test request status 200", async () => {
+    const response = await request(app).post("/login").send(data).expect(200);
+  });
 
+  test("test return 'token' and type of token is string", async () => {
     const response = await request(app).post("/login").send(data);
-    console.log(response);
-    expect(response.status).toBe(200);
+
+    expect(typeof response._body.token).toBe("string");
+  });
+
+  test("test return object 'user' with 2 properties: 'email', 'subscription'", async () => {
+    const response = await request(app).post("/login").send(data);
+
+    expect(typeof response._body.user).toBe("object");
+    expect(Object.keys(response._body.user)).toEqual(["email", "subscription"]);
+  });
+
+  test("test type of email and subscription is string", async () => {
+    const response = await request(app).post("/login").send(data);
+
+    expect(typeof response._body.user.email).toBe("string");
+    expect(typeof response._body.user.subscription).toBe("string");
   });
 });
